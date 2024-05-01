405c759fa315d2ba1e25e70bcb995022d5fdc80f
TME <tyler.s.meyers@gmail.com>
2024-05-01 07:54:18 -0600

    staleWhileRevalidating strategy

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require("workbox-recipes");
-const { CacheFirst } = require("workbox-strategies");
+const { CacheFirst, StaleWhileRevalidate } = require("workbox-strategies");
 const { registerRoute } = require("workbox-routing");
 const { CacheableResponsePlugin } = require("workbox-cacheable-response");
 const { ExpirationPlugin } = require("workbox-expiration");
@@ -9,6 +9,18 @@ precacheAndRoute(self.__WB_MANIFEST);
 
 const pageCache = new CacheFirst({
   cacheName: "page-cache",
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
+});
+
+const assetCache = new StaleWhileRevalidate({
+  cacheName: "asset-cache",
   plugins: [
     new CacheableResponsePlugin({
       statuses: [0, 200],
@@ -29,7 +41,7 @@ registerRoute(({ request }) => request.mode === "navigate", pageCache);
 // TODO: Implement asset caching
 registerRoute(
   ({ request }) => ["style", "script", "worker"].includes(request.destination),
-  new offlineFallback()
+  assetCache
 );
 
-
+offlineFallback({ urls: ["/index.html", "/"], strategy: assetCache });
